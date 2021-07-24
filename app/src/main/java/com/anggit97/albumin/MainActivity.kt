8f89098bf4da39f64a1bfe8bfe1d364f947db655
Anggit Prayogo <anggitprayogo@gmail.com>
2021-07-24 17:21:02 +0700

    show splash screen with lottie

--- app/src/main/java/com/anggit97/albumin/MainActivity.kt
+++ app/src/main/java/com/anggit97/albumin/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.anggit97.albumin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.anggit97.albumin.databinding.ActivityMainBinding
+import com.anggit97.core.util.viewBindings
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by viewBindings(ActivityMainBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            navigateToHome()
+        }, 2000)
+
+        setUpStatusBarColor()
+    }
+
+    private fun navigateToHome() {
+
+    }
+
+    private fun setUpStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(
+                applicationContext,
+                android.R.color.white
+            )
+        }
+        showDarkStatusBarIcons()
+    }
+
+    private fun showDarkStatusBarIcons() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility =
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
     }
 }
--- core/src/main/java/com/anggit97/core/notification/NotificationBuilder.kt
+++ core/src/main/java/com/anggit97/core/notification/NotificationBuilder.kt
@@ -1,12 +1,10 @@
 package com.anggit97.core.notification
 
-import com.anggit97.model.model.MovieDetail
-
 
 /**
  * Created by Anggit Prayogo on 15,July,2021
  * GitHub : https://github.com/anggit97
  */
 interface NotificationBuilder {
-    fun showReminderLatestMovie(movie: MovieDetail)
+//    fun showReminderLatestMovie(movie: MovieDetail)
 }
